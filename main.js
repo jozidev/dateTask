3a399913fc6466f8b5ffe5fcd5119bb1f16bc317
jozi <johan.b.strydom@gmail.com>
2022-05-20 20:58:55 +0200

    Remove task instructions

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-/*
- * Your program must print string with the number of years and months and the total number of days between the dates.
- * Dates are provided in dd.mm.yyyy format.
- * You are not allowed to plug in JS libraries such as moment.js or date-fns directly into the code. All code need to be written in this file.
- * 
- * Result must be shown as a string in years, months and total days. If years or months are 0, then it should not be displayed in the output.
- *
- * Example:
- * Input: ['01.01.2000', '01.01.2016']
- * Output:
- * '16 years, total 5844 days'
- *
- * Example 2:
- * Input: ['01.11.2015', '01.02.2017']
- *
- * Output:
- * '1 year, 3 months, total 458 days'
-*/
 const dates = [
     ['01.01.2000', '01.01.2016'],
     ['01.01.2016', '01.08.2016'],
